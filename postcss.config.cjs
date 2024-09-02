58beb1341f2a87122454a8a1c44896e18d553274
Zyy <No Email>
2024-09-02 13:03:18 +0800

    工程结构变更

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,5 +2,5 @@
 const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
-  plugin: [postcssPresetEnv()]
+  plugins: [postcssPresetEnv()]
 }
--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,5 +1,51 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>index</div>
+  <div class="aaa">
+    <input class="input" type="text" placeholder="请输入" />
+    <div class="autoplacement-example">
+      <div v-for="i in 10" :key="i" class="item"></div>
+    </div>
+  </div>
 </template>
+
+<style>
+body {
+  font-family: system-ui;
+}
+
+:root {
+  --mainColor: #12345678;
+}
+</style>
+
+<style lang="scss" scoped>
+.aaa {
+  font-family: system-ui;
+  color: var(--mainColor);
+  overflow-wrap: break-word;
+
+  .input::placeholder {
+    color: red;
+  }
+
+  .autoplacement-example {
+    display: flex;
+    justify-content: space-between;
+
+    .item {
+      width: 100px;
+      height: 100px;
+      margin: 2px;
+      background-color: #000;
+    }
+  }
+
+  // .autoplacement-example {
+  //   display: grid;
+  //   grid-template-rows: auto auto;
+  //   grid-template-columns: 1fr 1fr;
+  //   grid-gap: 20px;
+  // }
+}
+</style>
