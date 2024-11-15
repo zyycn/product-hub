cdf5e22f37b030d58c79114aa71dae31ac8d1ab9
Zyy <No Email>
2024-11-15 11:58:24 +0800

    更新项目配置

--- src/utils/element-plus-ui.ts
+++ src/utils/element-plus-ui.ts
@@ -1,6 +1,7 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/src/message.scss'
+import { App } from 'vue'
 
 export default {
   install(app: App, options = {}) {
