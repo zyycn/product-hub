a85a846cd732e697f75c09a0da56f21a727b920a
Zyy <No Email>
2024-11-21 14:52:31 +0800

    更新项目配置

--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import ProdConsole from '@/utils/prod-console'
 import Iconify from '@/utils/iconify'
 
 import 'normalize.css'
@@ -8,6 +9,7 @@ import 'virtual:uno.css'
 
 const app = createApp(App)
 
+app.use(ProdConsole)
 app.use(Iconify)
 
 app.mount('#app')
