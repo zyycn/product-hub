65fdcf036e85f1b39dd2fc4f61b7cd70aa5cd4f8
Zyy <No Email>
2024-11-23 15:09:18 +0800

    更新工程配置

--- config/mpa-config.ts
+++ config/mpa-config.ts
@@ -1,18 +1,6 @@
 import { program } from 'commander'
-import { MpaOptions, AllowedEvent, Page } from 'vite-plugin-virtual-mpa'
-
-const pages: Page[] = [
-  {
-    name: 'index',
-    entry: '/src/pages/index/main.ts',
-    data: { title: 'PROJECT PAGES（DEV）', icon: '/src/pages/index/public/favicon.svg' }
-  },
-  {
-    name: 'ops-tools',
-    entry: '/src/pages/ops-tools/main.ts',
-    data: { title: 'OPS Tools', icon: '/src/pages/ops-tools/public/favicon.svg' }
-  }
-]
+import { MpaOptions, AllowedEvent } from 'vite-plugin-virtual-mpa'
+import pages from './mpa-entry'
 
 // 多页面插件配置项
 const mpa_options: MpaOptions<string, string, string, AllowedEvent, string> = {
